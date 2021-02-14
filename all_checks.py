91a123373afdd82ba7ca3430ea7081dfa96d6b9c
MrbrownGB <abdelmohaymenbrown@gmail.com>
2021-02-14 11:25:50 +0200

    create wrapper function for check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -18,16 +18,21 @@ def check_disk_full(disk,min_GB,min_percent):
     if percent_free < min_percent or gigabites_free < min_GB :
         return True
     return False
+def check_full():
+    """return true if the root partion is full ,false otherwise"""
+    return check_disk_full(disk = '/', min_GB = 2 ,min_percent = 10)
 
 def main():
+    """there's a pattern of repeating code in our all checks pi script. For each check that we call, we check if it returns true or false.
+        When it returns true, we print an error and exit.  If we add a new check, we'll have to repeat this pattern again."""
     if check_reboot():
         print("pending reboot")
         sys.exit(1)
 
-    if check_disk_full(disk = '/', min_GB = 2 ,min_percent = 10):
-        print("Disk Full.")
+    if check_full():
+        print("root partion Full.")
         sys.exit(1)
 
-    print("okay")
+    print("Everything okay")
     sys.exit(0)
 main()
