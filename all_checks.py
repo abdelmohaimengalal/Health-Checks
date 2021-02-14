e0237718084a4b9e9e55652b2dbde52793072ef1
MrbrownGB <abdelmohaymenbrown@gmail.com>
2021-02-14 05:13:59 +0200

    add def check_disk_full func and make paramter clear to understand

--- all_checks.py
+++ all_checks.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 import os
 import sys
+import shutil
+
 def check_reboot():
     """return true if computer has pending rebot."""
     return os.path.exists("/run/reboot-required")
+
+
+def check_disk_full(disk,min_GB,min_percent):
+    """return true if there's not free disk space ,false otherwise"""
+    du = shutil.disk_usage(disk)
+    #calculate percentage of free space
+    percent_free = 100 * du.free /du.total
+    #calculate how many free gigabites
+    gigabites_free = du.free / 2**30
+    if percent_free < min_percent or gigabites_free < min_GB :
+        return True
+    return False
 
 def main():
     if check_reboot():
         print("pending reboot")
         sys.exit(1)
+
+    if check_disk_full(disk = '/', min_GB = 2 ,min_percent = 10):
+        print("Disk Full.")
+        sys.exit(1)
+
     print("okay")
     sys.exit(0)
 main()
